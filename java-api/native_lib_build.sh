307e7d690d14a21bb4c34e7a146e097ba2b72967
doki <tunyum94@gmail.com>
2022-07-04 09:53:16 +0800

    fix name

--- java-api/native_lib_build.sh
+++ java-api/native_lib_build.sh
@@ -8,12 +8,12 @@ cd ..
 echo "compile target=x86_64-unknown-linux-gnu"
 rustup target add x86_64-unknown-linux-gnu
 cross build --release --target=x86_64-unknown-linux-gnu
-cp -f target/x86_64-unknown-linux-gnu/release/libprql_java.so java-api/src/main/resources/libprql_java-linux64.so
+cp -f target/x86_64-unknown-linux-gnu/release/libprql_java.so java-api/src/main/resources/libprql4j-linux64.so
 
 echo "compile target=aarch64-unknown-linux-gnu"
 rustup target add aarch64-unknown-linux-gnu
 cross build --release --target=aarch64-unknown-linux-gnu
-cp -f target/aarch64-unknown-linux-gnu/release/libprql_java.so java-api/src/main/resources/libprql_java-linux-aarch64.so
+cp -f target/aarch64-unknown-linux-gnu/release/libprql_java.so java-api/src/main/resources/libprql4j-linux-aarch64.so
 
 #echo "compile target=x86_64-apple-darwin"
 #cross build --release --target=x86_64-apple-darwin
