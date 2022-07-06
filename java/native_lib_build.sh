a17b8eead0a615e2192bd505d0e612ecfac27969
doki <tunyum94@gmail.com>
2022-07-06 15:20:59 +0800

    test

--- java/native_lib_build.sh
+++ java/native_lib_build.sh
@@ -15,15 +15,15 @@ cp -f target/aarch64-unknown-linux-gnu/release/libprql4j.so java-api/src/main/re
 
 #echo "compile target=x86_64-apple-darwin"
 #cross build --release --target=x86_64-apple-darwin
-#cp -f target/x86_64-apple-darwin/release/libprql4j.dylib java-api/src/main/resources/libprql4j-osx-x86_64.dylib
+#cp -f target/x86_64-apple-darwin/release/libprql4j.dylib java/src/main/resources/libprql4j-osx-x86_64.dylib
 #
 #echo "compile target=aarch64-apple-darwin"
 #cross build --release --target=aarch64-apple-darwin
-#cp -f target/aarch64-apple-darwin/release/libprql4j.dylib java-api/src/main/resources/libprql4j-osx-arm64.dylib
+#cp -f target/aarch64-apple-darwin/release/libprql4j.dylib java/src/main/resources/libprql4j-osx-arm64.dylib
 #
 #echo "compile target=x86_64-pc-windows-gnu"
 #cross build --release --target=x86_64-pc-windows-gnu
-#cp -f target/x86_64-pc-windows-gnu/release/prql4j.dll java-api/src/main/resources/libprql4j-win64.dll
+#cp -f target/x86_64-pc-windows-gnu/release/prql4j.dll java/src/main/resources/libprql4j-win64.dll
 
 
 ### built by toolchain
@@ -31,24 +31,24 @@ cp -f target/aarch64-unknown-linux-gnu/release/libprql4j.so java-api/src/main/re
 #echo "compile target=x86_64-unknown-linux-gnu"
 #rustup target add x86_64-unknown-linux-gnu
 #cargo build --release --target=x86_64-unknown-linux-gnu
-#cp -f target/x86_64-unknown-linux-gnu/release/libprql4j.so java-api/src/main/resources/libprql4j-linux64.so
+#cp -f target/x86_64-unknown-linux-gnu/release/libprql4j.so java/src/main/resources/libprql4j-linux64.so
 #
 #echo "compile target=aarch64-unknown-linux-gnu"
 #rustup target add aarch64-unknown-linux-gnu
 #cargo build --release --target=aarch64-unknown-linux-gnu
-#cp -f target/aarch64-unknown-linux-gnu/release/libprql4j.so java-api/src/main/resources/libprql4j-linux-aarch64.so
+#cp -f target/aarch64-unknown-linux-gnu/release/libprql4j.so java/src/main/resources/libprql4j-linux-aarch64.so
 
 #echo "compile target=x86_64-apple-darwin"
 #rustup target add x86_64-apple-darwin
 #cargo build --release --target=x86_64-apple-darwin
-#cp -f target/x86_64-apple-darwin/release/libprql4j.dylib java-api/src/main/resources/libprql4j-osx-x86_64.dylib
+#cp -f target/x86_64-apple-darwin/release/libprql4j.dylib java/src/main/resources/libprql4j-osx-x86_64.dylib
 #
 #echo "compile target=aarch64-apple-darwin"
 #rustup target add aarch64-apple-darwin
 #cargo build --release --target=aarch64-apple-darwin
-#cp -f target/aarch64-apple-darwin/release/libprql4j.dylib java-api/src/main/resources/libprql4j-osx-arm64.dylib
+#cp -f target/aarch64-apple-darwin/release/libprql4j.dylib java/src/main/resources/libprql4j-osx-arm64.dylib
 #
 #echo "compile target=x86_64-pc-windows-gnu"
 #rustup target add x86_64-pc-windows-gnu
 #cargo build --release --target=x86_64-pc-windows-gnu
-#cp -f target/x86_64-pc-windows-gnu/release/prql4j.dll java-api/src/main/resources/libprql4j-win64.dll
+#cp -f target/x86_64-pc-windows-gnu/release/prql4j.dll java/src/main/resources/libprql4j-win64.dll
