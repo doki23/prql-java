ec889e7a4dc9e9a21ac2e2b8c73e65909bedfff3
doki <tunyum94@gmail.com>
2022-07-07 23:12:03 +0800

    update

--- cross.sh
+++ cross.sh
@@ -5,28 +5,28 @@ cargo install cross
 
 # x86_64-unknown-linux-gnu
 cross build --release --target x86_64-unknown-linux-gnu
-cp ../target/x86_64-unknown-linux-gnu/release/libprql_java.so java/src/main/resources/libprql_java-linux64.so
+cp ../target/x86_64-unknown-linux-gnu/release/libprql4j.so java/src/main/resources/libprql4j-linux64.so
 
 # x86_64-unknown-linux-musl
 cross build --release --target x86_64-unknown-linux-musl
-cp ../target/x86_64-unknown-linux-musl/release/libprql_java.so java/src/main/resources/libprql_java-linux64-musl.so
+cp ../target/x86_64-unknown-linux-musl/release/libprql4j.so java/src/main/resources/libprql4j-linux64-musl.so
 
 # x86_64-apple-darwin
 cross build --release --target x86_64-apple-darwin
-cp ../target/x86_64-apple-darwin/release/libprql_java.dylib java/src/main/resources/libprql_java-osx-x86_64.dylib
+cp ../target/x86_64-apple-darwin/release/libprql4j.dylib java/src/main/resources/libprql4j-osx-x86_64.dylib
 
 # x86_64-pc-windows-gnu
 cross build --release --target x86_64-pc-windows-gnu
-cp ../target/x86_64-pc-windows-gnu/release/prql_java.dll java/src/main/resources/libprql_java-win64.dylib
+cp ../target/x86_64-pc-windows-gnu/release/prql4j.dll java/src/main/resources/libprql4j-win64.dylib
 
 # aarch64-unknown-linux-gnu
 cross build --release --target aarch64-unknown-linux-gnu
-cp ../target/x86_64-unknown-linux-gnu/release/libprql_java.so java/src/main/resources/libprql_java-linux-aarch64.so
+cp ../target/x86_64-unknown-linux-gnu/release/libprql4j.so java/src/main/resources/libprql4j-linux-aarch64.so
 
 # aarch64-unknown-linux-musl
 cross build --release --target aarch64-unknown-linux-musl
-cp ../target/aarch64-unknown-linux-musl/release/libprql_java.so java/src/main/resources/libprql_java-linux-aarch64-musl.so
+cp ../target/aarch64-unknown-linux-musl/release/libprql4j.so java/src/main/resources/libprql4j-linux-aarch64-musl.so
 
 # aarch64-apple-darwin
 cross build --release --target aarch64-apple-darwin
-cp ../target/x86_64-apple-darwin/release/libprql_java.dylib java/src/main/resources/libprql_java-osx-arm64.dylib
+cp ../target/x86_64-apple-darwin/release/libprql4j.dylib java/src/main/resources/libprql4j-osx-arm64.dylib
